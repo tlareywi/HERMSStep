9932b67fb5f5453465e41fdc9f4b43792d00e287
Trystan Larey-Williams <trystan@bluecanvas-studios.com>
2020-08-19 09:47:46 -0700

    simplified and made so mash tun follows hysteresis logic

--- __init__.py
+++ __init__.py
@@ -16,8 +16,6 @@ class HERMSStep(StepBase):
     hlt = StepProperty.Kettle("HLT", description="Kettle used for heat transfer, hot liquor tank")
     timer = Property.Number("Rest (m)", configurable=True, description="Minutes of rest at target temp")
     hlt_offset = Property.Number("HLT Offset", configurable=True, default_value=8, description="Temp relative to mash target to maintain in HLT while rising")
-    pump = StepProperty.Actor("Recirculation Pump", description="Pump used to recirculate mash through HERMS coil")
-    ramp_next = 10 # Minutes before next step to apply heat back to HLT
 
     def init(self):
         '''
@@ -26,8 +24,7 @@ class HERMSStep(StepBase):
         '''
         # set target temp
         self.set_target_temp(self.target_temp, self.mash_tun)
-	self.set_target_temp(int(self.target_temp) + int(self.hlt_offset), self.hlt)
-        self.actor_on(int(self.pump))
+	    self.set_target_temp(int(self.target_temp) + int(self.hlt_offset), self.hlt)
 
     @cbpi.action("Start Timer Now")
     def start(self):
@@ -42,12 +39,11 @@ class HERMSStep(StepBase):
     def reset(self):
         self.stop_timer()
         self.set_target_temp(self.target_temp, self.mash_tun)
-	self.set_target_temp(int(self.target_temp) + int(self.hlt_offset), self.hlt)
+	    self.set_target_temp(int(self.target_temp) + int(self.hlt_offset), self.hlt)
 
     def finish(self):
         self.set_target_temp(0, self.mash_tun)
-	self.set_target_temp(0, self.hlt)
-        self.actor_off(int(self.pump))
+	    self.set_target_temp(0, self.hlt)
 
     def execute(self):
         '''
@@ -59,11 +55,6 @@ class HERMSStep(StepBase):
         if self.get_kettle_temp(self.mash_tun) >= int(self.target_temp):
             if self.is_timer_finished() is None:
                 self.start_timer(int(self.timer) * 60)
-		self.set_target_temp(0, self.mash_tun)
-		self.set_target_temp(0, self.hlt)
-
-	if int(self.timer_remaining()) <= int(self.ramp_next) * 60:
-	    self.set_target_temp(int(self.target_temp) + int(self.hlt_offset), self.hlt)
 
         # Check if timer finished and go to next step
         if self.is_timer_finished() == True:
